c072af1d29209077c4ca84b9d52b64be29709f1b
murloctidecaller <102994544+murloctidecaller@users.noreply.github.com>
2022-06-15 22:22:14 +0300

    fix test

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void shouldCalcExact() {
+        SQRService service = new SQRService();
+
+        int actual = service.calculate(200, 300);
+        int expected = 3;
+
+        assertEquals(expected, actual);
+    }
+}
+//
